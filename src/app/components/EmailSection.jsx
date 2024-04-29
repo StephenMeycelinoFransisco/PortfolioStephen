d5cfdcaf7a9609ba42da04614a68499cb323694a
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2024-04-29 23:47:18 +0700

    [add] link github&LinkedIn

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -59,10 +59,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/StephenMeycelinoFransisco?tab=repositories">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/stephenmf/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
